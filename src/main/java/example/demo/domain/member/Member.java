55038a4a231a5e66b189c493884378b4f3e5c63f
tkv00 <tkv0098@gmail.com>
2025-01-25 19:50:14 +0900

    [SCRUM-5] feat:member엔티티 생성,레포지토리 생성

--- src/main/java/example/demo/domain/member/Member.java
+++ src/main/java/example/demo/domain/member/Member.java
@@ -1,8 +1,31 @@
 package example.demo.domain.member;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
-
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @Column(name = "company_id")
+    private Long companyId;
+
+    @Column(name = "member_email")
+    private String email;
+
+    @Column(name = "member_name")
+    private String userName;
+
+    @Column(name = "member_password")
+    private String password;
+
+    @Column(name = "member_status")
+    @Enumerated(EnumType.STRING)
+    private MemberStatus memberStatus;
 }
---
+++ src/main/java/example/demo/domain/member/MemberRepositoryCustom.java
@@ -0,0 +1,7 @@
+package example.demo.domain.member;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepositoryCustom {
+}
