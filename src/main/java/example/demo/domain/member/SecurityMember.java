270d604f0a6d4a2e9abf35ca41feda59c22ecfff
tkv00 <tkv0098@gmail.com>
2025-01-28 20:21:13 +0900

    [SCRUM-5] feat:SecurityMember 엔티티 생성

---
+++ src/main/java/example/demo/domain/member/SecurityMember.java
@@ -0,0 +1,28 @@
+package example.demo.domain.member;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class SecurityMember implements UserDetails {
+    private final Member member;
+    @Override
+    public String getUsername() {
+        return member.getUserName();
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(()->member.getMemberStatus().toString());
+    }
+
+}
