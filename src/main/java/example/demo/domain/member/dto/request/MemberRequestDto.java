97976b23f9d17a451b96bd2a4e6e59cb5b924c15
tkv00 <tkv0098@gmail.com>
2025-01-29 22:43:44 +0900

    [SCRUM-5] refact:MemberRequestDto 생성자 수정

--- src/main/java/example/demo/domain/member/dto/request/MemberRequestDto.java
+++ src/main/java/example/demo/domain/member/dto/request/MemberRequestDto.java
@@ -8,6 +8,8 @@ import lombok.*;
 @Getter
 @Data
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
 public class  MemberRequestDto{
     @Pattern(regexp ="^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$")
     @NotBlank(message = "이메일은 필수 입력 값입니다.")
@@ -20,7 +22,6 @@ public class  MemberRequestDto{
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     private String password;
 
-    @NotBlank(message = "유저 형태는 필수 입력 값입니다.")
     private MemberStatus memberStatus;
 
     @Pattern(regexp = "^(010\\d{8}|\\d{3}\\d{7})$\n")
@@ -34,45 +35,41 @@ public class  MemberRequestDto{
     private String invitationCode;
 
     //일반인 사용자
-    @Builder
-    private MemberRequestDto(String email,String name,String password,String phoneNumber){
-        this.invitationCode=null;
-        this.companyPosition=null;
-        this.companyDept=null;
-        this.companyName=null;
-        this.memberStatus=MemberStatus.GENERAL;
-        this.password=password;
-        this.email=email;
-        this.name=name;
-        this.phoneNumber=phoneNumber;
+    public static MemberRequestDto ofGeneral(String email, String name, String password, String phoneNumber) {
+        return MemberRequestDto.builder()
+                .email(email)
+                .name(name)
+                .password(password)
+                .phoneNumber(phoneNumber)
+                .memberStatus(MemberStatus.GENERAL)
+                .build();
     }
+
     //회사 관리자 사용자
-    @Builder
-    private MemberRequestDto(String email,String name,String password,String phoneNumber,String companyName,String companyDept,String companyPosition){
-        this.companyName=companyName;
-        this.companyDept=companyDept;
-        this.companyPosition=companyPosition;
-        this.memberStatus=MemberStatus.MANAGER;
-        this.invitationCode=null;
-        this.password=password;
-        this.email=email;
-        this.name=name;
-        this.phoneNumber=phoneNumber;
+    public static MemberRequestDto ofManager(String email, String name, String password, String phoneNumber, String companyName, String companyDept, String companyPosition) {
+        return MemberRequestDto.builder()
+                .email(email)
+                .name(name)
+                .password(password)
+                .phoneNumber(phoneNumber)
+                .companyName(companyName)
+                .companyDept(companyDept)
+                .companyPosition(companyPosition)
+                .memberStatus(MemberStatus.MANAGER)
+                .build();
     }
 
     //회사 직원 사용자
-    @Builder
-    private MemberRequestDto(String email,String name,String password,String phoneNumber,String companyPosition,String invitationCode){
-        this.email=email;
-        this.name=name;
-        //TODO : 비밀번호 암호화 해야함.
-        this.password=password;
-        this.memberStatus=MemberStatus.EMPLOYEE;
-        this.phoneNumber=phoneNumber;
-        this.companyName=null;
-        this.companyDept=null;
-        this.companyPosition=companyPosition;
-        this.invitationCode=invitationCode;
+    public static MemberRequestDto ofEmployee(String email, String name, String password, String phoneNumber, String companyPosition, String invitationCode) {
+        return MemberRequestDto.builder()
+                .email(email)
+                .name(name)
+                .password(password)
+                .phoneNumber(phoneNumber)
+                .companyPosition(companyPosition)
+                .invitationCode(invitationCode)
+                .memberStatus(MemberStatus.EMPLOYEE)
+                .build();
     }
 
 }
--- src/test/java/example/demo/domain/member/dto/request/MemberRequestDtoTest.java
+++ src/test/java/example/demo/domain/member/dto/request/MemberRequestDtoTest.java
@@ -3,6 +3,7 @@ package example.demo.domain.member.dto.request;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class MemberRequestDtoTest {
@@ -28,6 +30,30 @@ class MemberRequestDtoTest {
     @AfterAll
     public static void close(){
         validatorFactory.close();
+    }
+
+    @Test
+    @DisplayName("회원가입 시 해당 유저 유형에 맞는 생성자를 통해 빌드합니다.")
+    void signupWithMemberStatus(){
+        //given  //when
+        MemberRequestDto general= MemberRequestDto.builder()
+                        .email("tkv00@naver.com")
+                        .phoneNumber("01050299737")
+                        .password("rlaehdus00!!")
+                        .name("member1")
+                        .build();
+        MemberRequestDto manager=MemberRequestDto.builder()
+                .email("tkv11@naver.com")
+                .phoneNumber("01050299999")
+                .password("rlaehdus11!!")
+                .name("member2")
+
+
+        //then
+        assertThat(general).extracting(
+                tuple("email","tkv00@naver.com"),
+                tuple()
+        )
     }
 
     @Test
