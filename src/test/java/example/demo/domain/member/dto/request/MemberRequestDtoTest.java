8b4735b2c893a1d9b2a643d7a4e86457455ba8b5
tkv00 <tkv0098@gmail.com>
2025-01-29 23:58:20 +0900

    [SCRUM-5] test:회원가입 비밀번호  유효성 검사 테스트 코드 작성

--- src/test/java/example/demo/domain/member/dto/request/MemberRequestDtoTest.java
+++ src/test/java/example/demo/domain/member/dto/request/MemberRequestDtoTest.java
@@ -105,7 +105,7 @@ class MemberRequestDtoTest {
     }
 
     @Test
-    @DisplayName("회원가입 시 이메일 유효성 검사를 실시합니다.")
+    @DisplayName("회원가입 시 이름 유효성 검사를 실시합니다.")
     void validationName(){
         //given
         MemberRequestDto general1=MemberRequestDto.ofGeneral(
@@ -133,6 +133,71 @@ class MemberRequestDtoTest {
         violations3.forEach(error ->
                 assertThat(error.getMessage()).isEqualTo("이름은 필수 입력 값입니다.")
         );
+    }
+
+    @Test
+    @DisplayName("회원가입 시 비밀번호 유효성 검사를 실시합니다.")
+    void validationPassword(){
+        //given
+        MemberRequestDto general1=MemberRequestDto.ofGeneral(
+                "tkv00@naver.com", "member1", null, "01050299737"
+        );
+        MemberRequestDto general2=MemberRequestDto.ofGeneral(
+                "tkv00@naver.com", "member1", "", "01050299737"
+        );
+        MemberRequestDto general3=MemberRequestDto.ofGeneral(
+                "tkv00@naver.com", "member1", " ", "01050299737"
+        );
+        //특수문자 사용X
+        MemberRequestDto general4=MemberRequestDto.ofGeneral(
+                "tkv00@naver.com", "member1", "rlaehdus00", "01050299737"
+        );
+        //8자미만
+        MemberRequestDto general5=MemberRequestDto.ofGeneral(
+                "tkv00@naver.com", "member1", "rlaehd", "01050299737"
+        );
+        //20자초과
+        MemberRequestDto general6=MemberRequestDto.ofGeneral(
+                "tkv00@naver.com", "member1", "rlaehdus00111111111111111111111111111111", "01050299737"
+        );
+        //영어 사용X 혹은 한글 포함
+        MemberRequestDto general7=MemberRequestDto.ofGeneral(
+                "tkv00@naver.com", "member1", "rlaehㅁㄴㅇㅁㄴㅇdus00", "01050299737"
+        );
+
+
+        //when
+        Set<ConstraintViolation<MemberRequestDto>> violations1 = getConstraintViolations(general1);
+        Set<ConstraintViolation<MemberRequestDto>> violations2 = getConstraintViolations(general2);
+        Set<ConstraintViolation<MemberRequestDto>> violations3 = getConstraintViolations(general3);
+        Set<ConstraintViolation<MemberRequestDto>> violations4 = getConstraintViolations(general4);
+        Set<ConstraintViolation<MemberRequestDto>> violations5 = getConstraintViolations(general5);
+        Set<ConstraintViolation<MemberRequestDto>> violations6 = getConstraintViolations(general6);
+        Set<ConstraintViolation<MemberRequestDto>> violations7 = getConstraintViolations(general7);
+
+
+        violations1.forEach(error ->
+                assertThat(error.getMessage()).isEqualTo("비밀번호는 필수 입력 값입니다.")
+        );
+        violations2.forEach(error ->
+                assertThat(error.getMessage()).isEqualTo("비밀번호는 필수 입력 값입니다.")
+        );
+        violations3.forEach(error ->
+                assertThat(error.getMessage()).isEqualTo("비밀번호는 필수 입력 값입니다.")
+        );
+        violations4.forEach(error->
+                assertThat(error.getMessage()).isEqualTo("잘못된 비밀번호 형식입니다.")
+        );
+        violations5.forEach(error->
+                assertThat(error.getMessage()).isEqualTo("잘못된 비밀번호 형식입니다.")
+        );
+        violations6.forEach(error->
+                assertThat(error.getMessage()).isEqualTo("잘못된 비밀번호 형식입니다.")
+        );
+        violations7.forEach(error->
+                assertThat(error.getMessage()).isEqualTo("잘못된 비밀번호 형식입니다.")
+        );
+
     }
 
 
