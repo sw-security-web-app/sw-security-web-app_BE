4cc4abc1fc786b64a23dbe3a3bc5796ecdad73be
tkv00 <tkv0098@gmail.com>
2025-02-06 04:58:58 +0900

    [SCRUM-25] faet:member 로그인 Dto 생성

---
+++ src/main/java/example/demo/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig  {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests((authorize)->authorize
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/example/demo/domain/member/dto/request/MemberLoginDto.java
@@ -0,0 +1,27 @@
+package example.demo.domain.member.dto.request;
+
+import example.demo.util.ValidationGroups;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class MemberLoginDto {
+    @NotBlank(message = "이메일은 필수 입력 값입니다.",
+            groups = ValidationGroups.NotEmptyGroup.class)
+    @Pattern(regexp ="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$",
+            message = "잘못된 이메일 입력입니다.",
+            groups = ValidationGroups.PatternCheckGroup.class
+    )
+    private String email;
+
+    private String password;
+
+    @Builder
+    public MemberLoginDto(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+}
