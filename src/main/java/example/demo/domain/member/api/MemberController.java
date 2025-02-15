1fc5828f158fda5ae59ae00c3442dd7660236011
tkv00 <tkv0098@gmail.com>
2025-02-15 20:35:54 +0900

    [SCURM-38] test:회원정보 반환 controller 테스트 코드 작성

--- src/main/java/example/demo/domain/member/api/MemberController.java
+++ src/main/java/example/demo/domain/member/api/MemberController.java
@@ -2,9 +2,11 @@ package example.demo.domain.member.api;
 
 import example.demo.domain.member.dto.request.MemberRequestDto;
 import example.demo.domain.member.dto.request.SmsCertificationRequestDto;
+import example.demo.domain.member.dto.response.MemberInfoResponseDto;
 import example.demo.util.ValidationGroups;
 import example.demo.util.ValidationSequence;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
@@ -18,6 +20,10 @@ public class MemberController {
     public ResponseEntity<?>signup(@Validated(ValidationSequence.class) @RequestBody MemberRequestDto memberRequestDto){
         memberService.signup(memberRequestDto);
         return ResponseEntity.ok("회원가입 성공");
+    }
+    @GetMapping("/api/my-info")
+    public ResponseEntity<MemberInfoResponseDto>info(@RequestHeader("Authorization") String token){
+        return new ResponseEntity<>(memberService.getMemberInfo(token), HttpStatus.OK);
     }
 
 
