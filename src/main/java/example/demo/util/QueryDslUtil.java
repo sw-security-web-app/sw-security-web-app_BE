6b1f2159ca6f78d984937fe242f339f9f9104b48
tkv00 <tkv0098@gmail.com>
2025-02-16 07:39:44 +0900

    [SCRUM-39] feat:queryDsl 가나다순 정렬

---
+++ src/main/java/example/demo/util/QueryDslUtil.java
@@ -0,0 +1,37 @@
+package example.demo.util;
+
+import com.querydsl.core.types.Expression;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.Expressions;
+import example.demo.domain.member.QMember;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class QueryDslUtil {
+    public static OrderSpecifier<?> getSortedColumn(Order order, Path<?> parent, String fieldName){
+        Path<Object> fieldPath= Expressions.path(Object.class,parent,fieldName);
+        return new OrderSpecifier(order,fieldPath);
+    }
+
+    //멤버의 이름으로 가나다순 정렬
+    public static List<OrderSpecifier> getAllOrderByMemberNameSpecifies(Pageable pageable,String entityType){
+        List<OrderSpecifier> orders=new ArrayList<>();
+
+        if(!pageable.getSort().isEmpty()){
+            for (Sort.Order order:pageable.getSort()){
+                Order direction=order.getDirection().isAscending()?Order.ASC:Order.DESC;
+                OrderSpecifier<?> orderSpecifier=new OrderSpecifier<>(direction, QMember.member.userName);
+                orders.add(orderSpecifier);
+            }
+        }else {
+            orders.add(QMember.member.userName.asc());
+        }
+        return orders;
+    }
+}
