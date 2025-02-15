2b6090eb70dbaeb2ebd55f566134573e05518e73
tkv00 <tkv0098@gmail.com>
2025-02-15 22:32:18 +0900

    [SCRUM-40] feat:회사명과 회사부서명으로 회사찾기 테스트 코드 작성

--- src/main/java/example/demo/domain/company/repository/CompanyRepository.java
+++ src/main/java/example/demo/domain/company/repository/CompanyRepository.java
@@ -5,6 +5,9 @@ import example.demo.domain.company.repository.CompanyCustomRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CompanyRepository extends JpaRepository<Company,Long> , CompanyCustomRepository {
+    Optional<Company> findByCompanyNameAndCompanyDept(String companyName,String companyDept);
 }
