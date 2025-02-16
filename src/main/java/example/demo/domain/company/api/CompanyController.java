86a7474364d67b5328fc5c24428c7a2065ad2b71
tkv00 <tkv0098@gmail.com>
2025-02-17 02:27:02 +0900

    [SCRUM-39] feat:member,company controller계층 메서드 구현

--- src/main/java/example/demo/domain/company/api/CompanyController.java
+++ src/main/java/example/demo/domain/company/api/CompanyController.java
@@ -1,22 +1,28 @@
 package example.demo.domain.company.api;
 
 import example.demo.domain.company.dto.CompanyCodeDto;
+import example.demo.domain.company.dto.response.CompanyResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class CompanyController {
     private final CompanyService companyService;
-    @GetMapping("/api/companycode")
+    @GetMapping("/company-code")
     public ResponseEntity<?> getCompanyCode(@RequestParam Long memberId){
         //TODO:나중에 JWT토큰으로 변경
         CompanyCodeDto codeDto=companyService.returnCompanyCode(memberId);
         return  ResponseEntity.status(HttpStatus.OK).body(new CompanyCodeDto(codeDto.getCompanyCode()));
     }
+
+    @GetMapping("/company-info")
+    public ResponseEntity<?> getCompanyInfo(@RequestHeader("Authorization")String token){
+        CompanyResponseDto companyInfo = companyService.getCompanyInfo(token);
+        return ResponseEntity.ok(companyInfo);
+    }
+
 }
--- src/main/java/example/demo/domain/member/api/MemberController.java
+++ src/main/java/example/demo/domain/member/api/MemberController.java
@@ -2,16 +2,18 @@ package example.demo.domain.member.api;
 
 import example.demo.domain.member.dto.request.MemberRequestDto;
 import example.demo.domain.member.dto.request.SmsCertificationRequestDto;
+import example.demo.domain.member.dto.response.CompanyEmployeeResponseDto;
 import example.demo.util.ValidationGroups;
 import example.demo.util.ValidationSequence;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,5 +24,11 @@ public class MemberController {
         memberService.signup(memberRequestDto);
         return ResponseEntity.ok("회원가입 성공");
     }
+    @GetMapping("/api/employee-list")
+    public ResponseEntity<?>getEmployees(@RequestHeader("Authorization")String token, @PageableDefault(sort = "name") Pageable pageable){
+        Page<CompanyEmployeeResponseDto> employees=memberService.getAllEmployees(token,pageable);
+        return ResponseEntity.ok(employees);
+    }
+
 
 }
