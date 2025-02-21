bcd7b34568c2d3f559c6b8a72f29840033e494a4
tkv00 <tkv0098@gmail.com>
2025-02-21 22:36:39 +0900

    [SCRUM-50] feat:refresh 반환 controller 구현

--- src/main/java/example/demo/security/auth/api/AuthApiController.java
+++ src/main/java/example/demo/security/auth/api/AuthApiController.java
@@ -1,5 +1,8 @@
 package example.demo.security.auth.api;
 
+import example.demo.domain.member.MemberErrorCode;
+import example.demo.error.RestApiException;
+import example.demo.security.auth.AuthErrorCode;
 import example.demo.security.auth.dto.MemberLoginDto;
 import example.demo.util.ValidationSequence;
 import jakarta.servlet.http.HttpServletResponse;
@@ -18,5 +21,15 @@ public class AuthApiController {
     public ResponseEntity<?> login(@Validated(ValidationSequence.class) @RequestBody MemberLoginDto loginDto, HttpServletResponse response){
         String token=authService.loginMember(loginDto,response);
         return ResponseEntity.ok().body(token);
+    }
+
+    @PostMapping("refresh")
+    public ResponseEntity<?> getRefreshToken(@CookieValue(name = "refreshToken",required = false)String refreshToken,
+                                             HttpServletResponse response){
+        if(refreshToken==null){
+            throw new RestApiException(AuthErrorCode.INVALID_REFRESH_TOKEN);
+        }
+        String newAccessToken= authService.refreshAccessToken(refreshToken,response);
+        return ResponseEntity.ok(newAccessToken);
     }
 }
