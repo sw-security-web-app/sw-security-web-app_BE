82e10b8473355d8cff99aa0cbb7b3dbb15119894
tkv00 <tkv0098@gmail.com>
2025-03-03 05:40:08 +0900

    [SCRUM-63]refact:예외처리 구체화

--- src/main/java/example/demo/verification/error/VerificationErrorCode.java
+++ src/main/java/example/demo/verification/error/VerificationErrorCode.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 public enum VerificationErrorCode implements ErrorCode {
     EMPTY_FILE_OR_TEXT(HttpStatus.BAD_REQUEST,"파일 또는 텍스트를 작성해주세요."),
     NO_FILE_TYPE(HttpStatus.BAD_REQUEST,"지원하지 않는 파일 유형입니다. (지원파일 유형 : .txt,pdf,.csv)"),
-    ERROR_OF_CREATE_COMPANY(HttpStatus.INTERNAL_SERVER_ERROR,"AI에게 파일 전송 중 오류가 발생했습니다.\n 다시 시도해주세요.");
+    ERROR_OF_CREATE_COMPANY(HttpStatus.INTERNAL_SERVER_ERROR,"회사 생성 중 오류가 발생했습니다."),
+    TIME_OUT_ERROR(HttpStatus.REQUEST_TIMEOUT,"요청 시간이 초과했습니다. 다시 시도해주세요."),
+    ERROR_OF_SEND_FILE_COMPANY(HttpStatus.INTERNAL_SERVER_ERROR,"회사 보안 데이터 전송 중 오류가 발생했습니다."),;
     private final HttpStatus httpStatus;
     private final String message;
 }
--- src/main/java/example/demo/verification/service/VerificationServiceImpl.java
+++ src/main/java/example/demo/verification/service/VerificationServiceImpl.java
@@ -14,7 +14,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.*;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
@@ -22,12 +21,11 @@ import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.UriComponentsBuilder;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 
 import java.io.IOException;
@@ -63,7 +61,16 @@ public class VerificationServiceImpl implements VerificationService {
         Long companyId = isMemberManager(token);
 
         //회사 생성
-        ResponseEntity<?> response = createCompany(companyId);
+        ResponseEntity<?> response ;
+        try {
+            response=createCompany(companyId);
+        }catch (ResourceAccessException e){
+            //회사 생성 시간 초과
+            throw new RestApiException(VerificationErrorCode.TIME_OUT_ERROR);
+        }catch (RestClientException e){
+            //회사 생성 중 통신 오류
+            throw new RestApiException(VerificationErrorCode.ERROR_OF_CREATE_COMPANY);
+        }
 
         //회사 생성 중 AI서버에서 오류 발생인 경우 예외처리
         if (response.getStatusCode().is4xxClientError()) {
@@ -74,6 +81,79 @@ public class VerificationServiceImpl implements VerificationService {
         String extractFileName = multipartFile.getOriginalFilename();
         body.add("company_name", companyId);
 
+        insertDataByFileType(multipartFile, requestDto, extractFileName, companyId, body);
+
+        SimpleClientHttpRequestFactory factory=new SimpleClientHttpRequestFactory();
+        RestTemplate restTemplate=new RestTemplate(factory);
+
+        factory.setConnectTimeout(5000);//연결 시간 5초
+        factory.setReadTimeout(5000);   //읽기 시간5초
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body);
+
+        ResponseEntity<String> request ;
+
+        try {
+            request=restTemplate.postForEntity(SERVER_URL + "/" + companyId + "/train"
+                    , requestEntity
+                    , String.class
+            );
+        }catch (ResourceAccessException e){
+            //요청시간 초과 시
+            throw new RestApiException(VerificationErrorCode.TIME_OUT_ERROR);
+        }catch (RestClientException e){
+            //요청 중 오류 발생 시
+            throw new RestApiException(VerificationErrorCode.ERROR_OF_SEND_FILE_COMPANY);
+        }
+
+        /*
+         * 전송 성공 유무 처리
+         * 성공 - 200 실패 - 422
+         * */
+        if (request.getStatusCode().is4xxClientError()) {
+            throw new RestApiException(VerificationErrorCode.ERROR_OF_CREATE_COMPANY);
+        }
+        return ResponseDto.of(200, "파일을 성공적으로 업로드하였습니다.");
+    }
+
+    //유저가 관리자 인지 검사 + 회사 인덱스 반환 메서드
+
+    private Long isMemberManager(String token) {
+        Long memberId = jwtUtil.getMemberId(token);
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new RestApiException(MemberErrorCode.MEMBER_NOT_FOUND));
+        if (!member.getMemberStatus().equals(MemberStatus.MANAGER)) {
+            throw new RestApiException(MemberErrorCode.INVALID_PERMISSION);
+        }
+
+        return member.getCompany().getCompanyId();
+    }
+    //확장자 명 추출 함수
+
+    private String extractFileType(String originalFileName) {
+        int pos = originalFileName.lastIndexOf(".");
+        return originalFileName.substring(pos + 1);
+    }
+    //파이썬 서버 회사 생성
+
+    private ResponseEntity<?> createCompany(Long companyId) {
+        Map<String, String> json = new HashMap<>();
+        json.put("company_name", String.valueOf(companyId));
+
+        //요청 만들기
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(json);
+        SimpleClientHttpRequestFactory factory=new SimpleClientHttpRequestFactory();
+        RestTemplate restTemplate=new RestTemplate(factory);
+
+        factory.setConnectTimeout(5000);//연결 시간 5초
+        factory.setReadTimeout(5000);   //읽기 시간5초
+
+        String url = SERVER_URL + "/create_company";
+        return restTemplate.postForEntity(url, request, String.class);
+    }
+
+    //파일 타입별 body조립 메서드
+    private void insertDataByFileType(MultipartFile multipartFile, SecurityFileRequestDto requestDto, String extractFileName, Long companyId, MultiValueMap<String, Object> body) throws IOException {
         //파일 타입 유형 검사
         if (!multipartFile.isEmpty()) {
             if (Arrays.stream(fileType).noneMatch(
@@ -102,62 +182,6 @@ public class VerificationServiceImpl implements VerificationService {
         else {
             body.add("text", requestDto.getContent());
         }
-        SimpleClientHttpRequestFactory factory=new SimpleClientHttpRequestFactory();
-        RestTemplate restTemplate=new RestTemplate(factory);
-
-        factory.setConnectTimeout(5000);//연결 시간 5초
-        factory.setReadTimeout(5000);   //읽기 시간5초
-
-        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body);
-
-        ResponseEntity<String> request = restTemplate.postForEntity(SERVER_URL + "/" + companyId + "/train"
-                , requestEntity
-                , String.class
-        );
-
-        /*
-         * 전송 성공 유무 처리
-         * 성공 - 200 실패 - 422
-         * */
-        if (request.getStatusCode().is4xxClientError()) {
-            throw new RestApiException(VerificationErrorCode.ERROR_OF_CREATE_COMPANY);
-        }
-        return ResponseDto.of(200, "파일을 성공적으로 업로드하였습니다.");
-    }
-
-    //유저가 관리자 인지 검사 + 회사 인덱스 반환 메서드
-    private Long isMemberManager(String token) {
-        Long memberId = jwtUtil.getMemberId(token);
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new RestApiException(MemberErrorCode.MEMBER_NOT_FOUND));
-        if (!member.getMemberStatus().equals(MemberStatus.MANAGER)) {
-            throw new RestApiException(MemberErrorCode.INVALID_PERMISSION);
-        }
-
-        return member.getCompany().getCompanyId();
-    }
-
-    //확장자 명 추출 함수
-    private String extractFileType(String originalFileName) {
-        int pos = originalFileName.lastIndexOf(".");
-        return originalFileName.substring(pos + 1);
-    }
-
-    //파이썬 서버 회사 생성
-    private ResponseEntity<?> createCompany(Long companyId) {
-        Map<String, String> json = new HashMap<>();
-        json.put("company_name", String.valueOf(companyId));
-
-        //요청 만들기
-        HttpEntity<Map<String, String>> request = new HttpEntity<>(json);
-        SimpleClientHttpRequestFactory factory=new SimpleClientHttpRequestFactory();
-        RestTemplate restTemplate=new RestTemplate(factory);
-
-        factory.setConnectTimeout(5000);//연결 시간 5초
-        factory.setReadTimeout(5000);   //읽기 시간5초
-
-        String url = SERVER_URL + "/create_company";
-        return restTemplate.postForEntity(url, request, String.class);
     }
 
 }
