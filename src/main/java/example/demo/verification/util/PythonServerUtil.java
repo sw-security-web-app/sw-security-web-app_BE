e651c6211b3ee085c96d39451d5271e6df743d2a
tkv00 <tkv0098@gmail.com>
2025-03-06 23:27:01 +0900

    [SCRUM-78] refact:Python Util 로직 수정

--- src/main/java/example/demo/verification/util/PythonServerUtil.java
+++ src/main/java/example/demo/verification/util/PythonServerUtil.java
@@ -1,7 +1,9 @@
 package example.demo.verification.util;
 
+import example.demo.domain.member.MemberStatus;
 import example.demo.error.RestApiException;
 import example.demo.util.UtilErrorCode;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
@@ -13,6 +15,7 @@ import java.util.Map;
 import static org.springframework.web.reactive.function.client.WebClient.*;
 
 @Component
+@Slf4j
 public class PythonServerUtil {
 
     private final WebClient webClient;
@@ -24,20 +27,32 @@ public class PythonServerUtil {
         this.webClient = webClient.build();
     }
 
-    public void validatePrompt(String prompt) {
+    public void validatePrompt(String prompt, Long companyId) {
         Map<String, String> requestBody = Map.of("prompt", prompt);
+        HttpStatusCode responseStatus = null;
 
-        HttpStatusCode responseStatus = webClient.post()
-                .uri(pythonServerUrl + "/predict")
-                .header("Content-Type", "application/json")
-                .bodyValue(requestBody)
-                .exchangeToMono(response -> Mono.just(response.statusCode()))
-                .block();
+        //일반인
+        if (companyId.equals(0L)) {
+            responseStatus=webClient.post()
+                    .uri(pythonServerUrl +  "/predict")
+                    .header("Content-Type", "application/json")
+                    .bodyValue(requestBody)
+                    .exchangeToMono(response -> Mono.just(response.statusCode()))
+                    .block();
+        }else {
+            responseStatus=webClient.post()
+                    .uri(pythonServerUrl + "/" + companyId + "/predict")
+                    .header("Content-Type", "application/json")
+                    .bodyValue(requestBody)
+                    .exchangeToMono(response -> Mono.just(response.statusCode()))
+                    .block();
+        }
 
         if (responseStatus == null) {
             throw new RestApiException(UtilErrorCode.PYTHON_SERVER_ERROR);
         } else if (!responseStatus.is2xxSuccessful()) {
-            throw new RestApiException(UtilErrorCode.PYTHON_SERVER_ERROR);
+            log.error("❌회사 보안 위반!❌");
+            throw new RestApiException(UtilErrorCode.NOT_ALLOWED_QUESTION);
         }
     }
 }
